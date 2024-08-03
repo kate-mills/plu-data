d2574b232e339941ac1785810675e081830b1d28
Kate Mills <helloworldloveksm@icloud.com>
2024-08-03 16:38:27 -0700

    iterable data

---
+++ pluData.js
@@ -0,0 +1,397 @@
+export const pluData = [
+  {
+    name: 'Envy Apple',
+    category: 'Apples',
+    plu: 3315,
+  },
+  {
+    name: 'Fuji Apple',
+    category: 'Apples',
+    plu: 4131,
+  },
+  {
+    name: 'Gala Apple',
+    category: 'Apples',
+    plu: 4135,
+  },
+  {
+    name: 'Granny Smith Apple',
+    category: 'Apples',
+    plu: 4017,
+  },
+  {
+    name: 'Honey Crisp Apple',
+    category: 'Apples',
+    plu: 3283,
+  },
+  {
+    name: 'Opal Apple',
+    category: 'Apples',
+    plu: 3618,
+  },
+  {
+    name: 'Pink Lady Apple',
+    category: 'Apples',
+    plu: 4130,
+  },
+  {
+    name: 'Kiku Apple',
+    category: 'Apples',
+    plu: 3613,
+  },
+  {
+    name: 'Pinata Apple',
+    category: 'Apples',
+    plu: 3435,
+  },
+  {
+    name: 'Lemon',
+    category: 'Citrus',
+    plu: 4053,
+  },
+  {
+    name: 'Lime',
+    category: 'Citrus',
+    plu: 4048,
+  },
+  {
+    name: 'Grapefruit',
+    category: 'Citrus',
+    plu: 4286,
+  },
+  {
+    name: 'Orange',
+    category: 'Citrus',
+    plu: 4012,
+  },
+  {
+    name: 'Sumo',
+    category: 'Citrus',
+    plu: 3632,
+  },
+  {
+    name: 'Pomello',
+    category: 'Citrus',
+    plu: 3129,
+  },
+  {
+    name: 'Banana',
+    category: 'Tropical',
+    plu: 4011,
+  },
+  {
+    name: 'Kiwi',
+    category: 'Tropical',
+    plu: 4030,
+  },
+  {
+    name: 'Cantaloupe',
+    category: 'Melons',
+    plu: 4050,
+  },
+  {
+    name: 'Tuscan',
+    category: 'Melons',
+    plu: 4332,
+  },
+  {
+    name: 'Galia',
+    category: 'Melons',
+    plu: 3246,
+  },
+  {
+    name: 'Honeydew',
+    category: 'Melons',
+    plu: 4329,
+  },
+  {
+    name: 'Watermelon',
+    category: 'Melons',
+    plu: 4032,
+  },
+  {
+    name: 'Mini Watermelon',
+    category: 'Melons',
+    plu: 3421,
+  },
+  {
+    name: 'Mango',
+    category: 'Tropical',
+    plu: 4959,
+  },
+  {
+    name: 'Mango Gold',
+    category: 'Tropical',
+    plu: 4312,
+  },
+  {
+    name: 'Dragon Fruit',
+    category: 'Tropical',
+    plu: 3319,
+  },
+  {
+    name: 'Asian Pear',
+    category: 'Pears',
+    plu: 4408,
+  },
+  {
+    name: 'Bosc Pear',
+    category: 'Pears',
+    plu: 4413,
+  },
+  {
+    name: 'Bartlett Pear',
+    category: 'Pears',
+    plu: 4024,
+  },
+  {
+    name: "D'Anjou Pear",
+    category: 'Pears',
+    plu: 4416,
+  },
+  {
+    name: 'Red Pear',
+    category: 'Pears',
+    plu: 4417,
+  },
+  {
+    name: 'Papaya',
+    category: 'Tropical',
+    plu: 3312,
+  },
+  {
+    name: 'Pineapple',
+    category: 'Tropical',
+    plu: 4430,
+  },
+  {
+    name: 'Pom',
+    category: 'Tropical',
+    plu: 3127,
+  },
+  {
+    name: 'White Nectarine',
+    category: 'Stone Fruit',
+    plu: 3035,
+  },
+  {
+    name: 'Yellow Nectarine',
+    category: 'Stone Fruit',
+    plu: 4378,
+  },
+  {
+    name: 'White Peach',
+    category: 'Stone Fruit',
+    plu: 4044,
+  },
+  {
+    name: 'Persimmon',
+    category: 'Stone Fruit',
+    plu: 4427,
+  },
+  {
+    name: 'Plum',
+    category: 'Stone Fruit',
+    plu: 4040,
+  },
+  {
+    name: 'Plumcot Fruit',
+    category: 'Stone Fruit',
+    plu: 3278,
+  },
+  {
+    name: 'Red Pepper',
+    category: 'Peppers',
+    plu: 4688,
+  },
+  {
+    name: 'Orange Pepper',
+    category: 'Peppers',
+    plu: 3121,
+  },
+  {
+    name: 'Yellow Pepper',
+    category: 'Peppers',
+    plu: 4689,
+  },
+  {
+    name: 'Green Pepper',
+    category: 'Peppers',
+    plu: 4065,
+  },
+  {
+    name: 'Red Onion',
+    category: 'Aromatics',
+    plu: 4082,
+  },
+  {
+    name: 'Sweet Onion',
+    category: 'Aromatics',
+    plu: 4166,
+  },
+  {
+    name: 'Yellow Onion',
+    category: 'Aromatics',
+    plu: 4093,
+  },
+  {
+    name: 'White Onion',
+    category: 'Aromatics',
+    plu: 4663,
+  },
+  {
+    name: 'Shallot',
+    category: 'Aromatics',
+    plu: 4662,
+  },
+  {
+    name: 'Garlic',
+    category: 'Aromatics',
+    plu: 4608,
+  },
+  {
+    name: 'Gold Potato',
+    category: 'Potatoes',
+    plu: 4727,
+  },
+  {
+    name: 'Red Potato',
+    category: 'Potatoes',
+    plu: 4073,
+  },
+  {
+    name: 'Russet Potato',
+    category: 'Potatoes',
+    plu: 4072,
+  },
+  {
+    name: 'Sweet Potato',
+    category: 'Potatoes',
+    plu: 4817,
+  },
+  {
+    name: 'Japanese Potato',
+    category: 'Potatoes',
+    plu: 3288,
+  },
+  {
+    name: 'Avocado',
+    category: 'Others',
+    plu: 4225,
+  },
+  {
+    name: 'Asparagus',
+    category: 'Others',
+    plu: 4080,
+  },
+  {
+    name: 'Brussel',
+    category: 'Others',
+    plu: 3083,
+  },
+  {
+    name: 'Artichoke',
+    category: 'Others',
+    plu: 4084,
+  },
+  {
+    name: 'Corn',
+    category: 'Others',
+    plu: 4077,
+  },
+  {
+    name: 'Cucumber',
+    category: 'Others',
+    plu: 4593,
+  },
+  {
+    name: 'Eggplant',
+    category: 'Others',
+    plu: 4081,
+  },
+  {
+    name: 'Jalapeno',
+    category: 'Others',
+    plu: 4693,
+  },
+  {
+    name: 'Acorn Squash',
+    category: 'Squash',
+    plu: 4750,
+  },
+  {
+    name: 'Butternut Squash',
+    category: 'Squash',
+    plu: 4759,
+  },
+  {
+    name: 'Delicata Squash',
+    category: 'Squash',
+    plu: 4763,
+  },
+  {
+    name: 'Spaghetti Squash',
+    category: 'Squash',
+    plu: 4776,
+  },
+  {
+    name: 'Zucchini Squash',
+    category: 'Squash',
+    plu: 4067,
+  },
+  {
+    name: 'Zucchini Yellow Squash',
+    category: 'Squash',
+    plu: 4782,
+  },
+  {
+    name: 'Pie Pumpkin',
+    category: 'Pumpkins',
+    plu: 3134,
+  },
+  {
+    name: 'Ornamental Pumpkin',
+    category: 'Pumpkins',
+    plu: 95587,
+  },
+  {
+    name: 'Tiger Stripe Pumpkin',
+    category: 'Pumpkins',
+    plu: 6715,
+  },
+  {
+    name: 'Fantasy Pumpkin',
+    category: 'Pumpkins',
+    plu: 4480,
+  },
+  {
+    name: 'Autumn Mini Pumpkin',
+    category: 'Pumpkins',
+    plu: 95582,
+  },
+  {
+    name: 'Mini White Pumpkin',
+    category: 'Pumpkins',
+    plu: 97806,
+  },
+  {
+    name: 'Mini Orange Pumpkin',
+    category: 'Pumpkins',
+    plu: 4734,
+  },
+  {
+    name: 'Orange Pumpkin',
+    category: 'Pumpkins',
+    plu: 4735,
+  },
+  {
+    name: 'Beefsteak Tomato',
+    category: 'Tomatoes',
+    plu: 4799,
+  },
+  {
+    name: 'Roma Tomato',
+    category: 'Tomatoes',
+    plu: 4087,
+  },
+]
